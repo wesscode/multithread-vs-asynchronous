4ea724af2892a8d955d6054bc1faff83032d834b
wesscode <wesley.info07@gmail.com>
2025-03-01 19:11:45 -0300

    feat: implementation multithread

--- MultithreadVsAsynchrono/Multithread.cs
+++ MultithreadVsAsynchrono/Multithread.cs
@@ -2,5 +2,30 @@
 {
     internal class Multithread
     {
+        internal static void HardProcess(object id)
+        {
+            Console.WriteLine($"Thread {id} started.");
+            Thread.Sleep(2000);  // simulating hard process (calc, process img, etc.)
+            Console.WriteLine($"Thread {id} ended.");
+        }
+
+        /// <summary>
+        /// - O que acontece aqui?
+        ///   - Criamos 5 threads.
+        ///   - Cada thread simula um trabalho pesado.
+        ///   - As threads rodam em paralelo (se tiver núcleos disponíveis).
+        ///   - A thread principal(execute) fica esperando.
+        /// </summary>
+        public static void Execute()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var thread = new Thread(HardProcess!);
+                thread.Start(i);
+            }
+
+            Console.WriteLine("Execute thread continue...");
+            Console.ReadLine();
+        }
     }
 }
